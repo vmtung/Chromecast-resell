05c4a4c9100105e617a497b2b32e1882cfa39be5
Tung <vmtung93@gmail.com>
2020-02-23 22:15:34 +0700

    remove old JS code

--- js/init.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var paralaxElems = document.querySelectorAll('.parallax');
-  M.Parallax.init(paralaxElems);
-
-  var sideNavElems = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(sideNavElems);
-});
